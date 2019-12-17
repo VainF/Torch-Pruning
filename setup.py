b171f2785c237dee7335816f171dfb786c4301e5
VainF <2218880241@qq.com>
2019-12-17 16:34:33 +0000

    0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="torch_pruning",
-    version="0.1.0",
+    version="0.1.1",
     author="Gongfan Fang",
     author_email="fgf@zju.edu.cn",
     description="A pytorch toolkit for neural network pruning and layer dependency maintaining.",
