4f1f3c555196d5e8f14eeb8aae8b99d761502fe6
VainF <2218880241@qq.com>
2021-01-29 22:28:11 +0800

    v0.2.3: support L1/L2/random strategy

--- examples/dependency_graph.py
+++ examples/dependency_graph.py
@@ -12,7 +12,8 @@ strategy = tp.strategy.L1Strategy() # or tp.strategy.RandomStrategy()
 DG = tp.DependencyGraph()
 DG.build_dependency(model, example_inputs=torch.randn(1,3,224,224))
 # get a pruning plan according to the dependency graph. idxs is the indices of pruned filters.
-pruning_plan = DG.get_pruning_plan( model.conv1, tp.prune_conv, idxs=strategy(model.conv1.weight, amount=0.1) )
+pruning_idxs = strategy(model.conv1.weight, amount=0.4) # or manually selected [0, 2, 6]
+pruning_plan = DG.get_pruning_plan( model.conv1, tp.prune_conv, idxs=pruning_idxs )
 print(pruning_plan)
 # execute this plan (prune the model)
 pruning_plan.exec()
