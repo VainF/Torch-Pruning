158c06241251cc1ca0b3cf1d0175201f243acda1
Sergei Garkusha <viur90@gmail.com>
2021-07-05 11:11:25 +0300

    adding rounding to pruning strategy

--- torch_pruning/prune/strategy.py
+++ torch_pruning/prune/strategy.py
@@ -8,18 +8,19 @@ class BaseStrategy(ABC):
         return self.apply(weights, amount=amount)
 
     @abstractclassmethod
-    def apply(self, weights, amount=0.0)->  Sequence[int]:  # return index
+    def apply(self, weights, amount=0.0, round_to=1)->  Sequence[int]:  # return index
         """ Apply the strategy on weights with user specified pruning percentage.
 
         Parameters:
             weights (torch.Parameter): weights to be pruned.
             amount (Callable): the percentage of weights to be pruned.
+            round_to (int): the number to which the number of pruned channels is rounded.
         """
         raise NotImplementedError
 
 class RandomStrategy(BaseStrategy):
 
-    def apply(self, weights, amount=0.0)->  Sequence[int]:  # return index
+    def apply(self, weights, amount=0.0, round_to=1)->  Sequence[int]:  # return index
         if amount<=0: return []
         n = len(weights)
         n_to_prune = int(amount*n)
@@ -30,11 +31,19 @@ class LNStrategy(BaseStrategy):
     def __init__(self, p):
         self.p = p
 
-    def apply(self, weights, amount=0.0)->  Sequence[int]:  # return index
+    def apply(self, weights, amount=0.0, round_to=1)->  Sequence[int]:  # return index
         if amount<=0: return []
         n = len(weights)
         l1_norm = torch.norm( weights.view(n, -1), p=self.p, dim=1 )
-        n_to_prune = int(amount*n)
+        if round_to == 1:
+            n_to_prune = int(amount*n)
+        else:
+            if n <= round_to:
+                n_to_prune = 0
+                print("Warning, initial number of channels is less than `round_to` parameter.")
+            else:
+                remainder = n % round_to
+                n_to_prune = remainder + round(amount*n/round_to)*round_to
         if n_to_prune == 0:
             return []
         threshold = torch.kthvalue(l1_norm, k=n_to_prune).values 
