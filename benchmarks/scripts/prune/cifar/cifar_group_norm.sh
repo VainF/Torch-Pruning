42fff081f945198accf785bdacf89883381b8317
VainF <2218880241@qq.com>
2022-10-27 15:25:32 +0800

    update

--- benchmarks/scripts/prune/cifar/cifar_group_norm.sh
+++
@@ -1,7 +0,0 @@
-python main.py --mode prune --model resnet56 --batch_size 128 --restore run/cifar10/pretrain/cifar10_resnet56.pth --dataset cifar10  --method group_norm --speed_up 2.1 --soft_rank 0.0 --global_pruning
-python main.py --mode prune --model resnet56 --batch_size 128 --restore run/cifar10/pretrain/cifar10_resnet56.pth --dataset cifar10  --method group_norm --speed_up 2.5 --soft_rank 0.0 --global_pruning
-python main.py --mode prune --model vgg19 --batch_size 128 --restore run/cifar100/pretrain/cifar100_vgg19.pth --dataset cifar100  --method group_norm --speed_up 8.84 --soft_rank 0.1 --global_pruning
-
-#python main.py --mode prune --model resnet56 --batch_size 128 --restore run/cifar10/pretrain/cifar10_resnet56.pth --dataset cifar10  --method group_sl --speed_up 2.1 --soft_rank 0.0 --global_pruning
-#python main.py --mode prune --model resnet56 --batch_size 128 --restore run/cifar10/pretrain/cifar10_resnet56.pth --dataset cifar10  --method group_sl --speed_up 2.55 --soft_rank 0.0 --global_pruning
-#python main.py --mode prune --model vgg19 --batch_size 128 --restore run/cifar100/pretrain/cifar100_vgg19.pth --dataset cifar100  --method group_sl --speed_up 8.84 --soft_rank 0.1 --global_pruning
---
+++ benchmarks/scripts/prune/cifar/full_group_norm.sh
@@ -0,0 +1,5 @@
+python main.py --mode prune --model resnet56 --batch-size 128 --restore run/cifar10/pretrain/cifar10_resnet56.pth --dataset cifar10  --method group_norm --speed-up 2.11 --global-pruning --reg 5e-4 --max-sparsity 0.75
+
+python main.py --mode prune --model resnet56 --batch-size 128 --restore run/cifar10/pretrain/cifar10_resnet56.pth --dataset cifar10  --method group_norm --speed-up 2.55 --global-pruning --reg 5e-4 --max-sparsity 0.75
+
+python main.py --mode prune --model vgg19 --batch-size 128 --restore run/cifar100/pretrain/cifar100_vgg19.pth --dataset cifar100  --method group_norm --speed-up 8.84 --global-pruning --reg 5e-4 --max-sparsity 0.75
