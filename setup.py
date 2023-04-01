20ddbc736c1fedbfa60afb73ed124b080bd6f867
Gongfan Fang <gongfan@u.nus.edu>
2023-04-02 03:30:43 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="torch-pruning",
-    version="v1.1.1",
+    version="v1.1.2",
     author="Gongfan Fang",
     author_email="gongfan@u.nus.edu",
     description="Structural Pruning for Model Acceleration.",
