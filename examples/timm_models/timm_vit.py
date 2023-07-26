71028c146a10b2757f0d7e5e60d0faf7a19f5445
VainF <2218880241@qq.com>
2023-07-26 18:46:06 +0800

    An example for Timm ViT

--- examples/timm_models/timm_vit.py
+++ examples/timm_models/timm_vit.py
@@ -68,26 +68,6 @@ problem_with_input_shape = []
 
 timm_atten_pruner = TimmAttentionPruner()
 
-
-from transformers import ViTImageProcessor, ViTForImageClassification
-from PIL import Image
-import requests
-
-url = 'http://images.cocodataset.org/val2017/000000039769.jpg'
-image = Image.open(requests.get(url, stream=True).raw)
-
-processor = ViTImageProcessor.from_pretrained('google/vit-base-patch16-224')
-model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224')
-print(model)
-inputs = processor(images=image, return_tensors="pt")
-outputs = model(**inputs)
-logits = outputs.logits
-# model predicts one of the 1000 ImageNet classes
-predicted_class_idx = logits.argmax(-1).item()
-print("Predicted class:", model.config.id2label[predicted_class_idx])
-
-
-
 for i, model_name in enumerate(timm_models):
     if not model_name=='vit_base_patch8_224':
         continue
