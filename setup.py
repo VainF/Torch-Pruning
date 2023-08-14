2671e128676b7705ba3be1d8afbb6bb8b7f858fe
VainF <2218880241@qq.com>
2023-08-14 15:23:20 +0800

    v1.2.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="torch-pruning",
-    version="v1.2.1",
+    version="v1.2.2",
     author="Gongfan Fang",
     author_email="gongfan@u.nus.edu",
     description="Towards Any Structural Pruning",
