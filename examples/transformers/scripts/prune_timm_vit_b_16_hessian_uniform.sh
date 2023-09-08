f28de040f30813ed9056c38c2ec40ac343878bc7
VainF <2218880241@qq.com>
2023-09-09 02:24:24 +0800

    Add hessian-based ViT pruning

---
+++ examples/transformers/scripts/prune_timm_vit_b_16_hessian_uniform.sh
@@ -0,0 +1,10 @@
+python prune_timm_vit.py \
+    --model_name vit_base_patch16_224 \
+    --pruning_type hessian \
+    --pruning_ratio 0.5 \
+    --taylor_batchs 10 \
+    --test_accuracy \
+    --data_path ~/Datasets/shared/imagenet \
+    --train_batch_size 64 \
+    --val_batch_size 64 \
+    --save_as output/pruned/vit_base_patch16_224_pruned_hessian_uniform.pth \
