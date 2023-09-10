080c0359b8c1e39900d9611a2caee96ef7bd4ab9
VainF <2218880241@qq.com>
2023-09-10 23:32:35 +0800

    Update draw_acc_curve.py

--- examples/transformers/draw_acc_curve.py
+++ examples/transformers/draw_acc_curve.py
@@ -12,26 +12,17 @@ def parse_acc_from_file(file_path):
             acc.append(float(line.split(' ')[-3]))
     return acc
 
-acc_taylor_bottleneck = parse_acc_from_file('output/vit_b_16_taylor_pruning_taylor_bottleneck/train.log')
-acc_taylor_uniform = parse_acc_from_file('output/vit_b_16_pruning_taylor_uniform/train.log')
-acc_l1_uniform = parse_acc_from_file('output/vit_b_16_pruning_l1_uniform/train.log')
+log_dict = {
+    'Hessian-uniform': 'output/vit_b_16_pruning_hessian_uniform/train.log',
+    'Taylor-uniform': 'output/vit_b_16_pruning_taylor_uniform/train.log',
+    'L1-uniform': 'output/vit_b_16_pruning_l1_uniform/train.log',
+}
 
-print("[Final]")
-print("acc_taylor_bottleneck:", acc_taylor_bottleneck[-1])
-print("acc_taylor_uniform:", acc_taylor_uniform[-1])
-print("acc_l1_uniform:", acc_l1_uniform[-1])
-print("")
-print("[0 Ep]")
-print("acc_taylor_bottleneck:", acc_taylor_bottleneck[0])
-print("acc_taylor_uniform:", acc_taylor_uniform[0])
-print("acc_l1_uniform:", acc_l1_uniform[0])
-
-#draw fig
 plt.figure(figsize=(8, 4), dpi=200)
-plt.plot(acc_taylor_bottleneck, label='Taylor-bottleneck')
-plt.plot(acc_taylor_uniform, label='Taylor-uniform')
-plt.plot(acc_l1_uniform, label='L1-uniform')
-                                                                                                                                                                                                                                                                                                                                                                                                                    #plt.plot(acc_random, label='Random-uniform')rplt.xlabel('Epoch')
+for exp_name, log_path in log_dict.items():
+    acc = parse_acc_from_file(log_path)
+    plt.plot(acc, label=exp_name)
+                                                                                                                                                                                                                                                                                                                                                                                                       #plt.plot(acc_random, label='Random-uniform')rplt.xlabel('Epoch')
 plt.ylabel('Accuracy')
 plt.legend()
 plt.legend(loc='lower right')
