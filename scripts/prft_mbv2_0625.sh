48649ace5d2a42d6f6cef77916b1d0845434d5d9
liu-mengyang <myliu@seu.edu.cn>
2024-06-01 19:33:05 +0000

    Fix typo

--- scripts/prft_mbv2_0625.sh
+++ scripts/prft_mbv2_0625.sh
@@ -1,5 +1,4 @@
 python prune_timm_models.py 0.625 --name mobilenetv2_100 --exp_name mbv2_0625
-0125
 
 torchrun --nproc_per_node=8 --master_port=25678 finetune_timm_models.py \
         --path /lmy/datasets/imagenet \
